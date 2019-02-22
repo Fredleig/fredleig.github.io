ec6be936c70add2d72fc9cc47f9b1361fcd1c85a
fredleig <Kentis-can@protonmail.com>
2019-02-22 15:45:23 +0300

    1

--- work-projects/tobacco/js/modalWindow.js
+++
@@ -1,3 +0,0 @@
-//* Modal Window
-const modalWindow=function(){let a=document.body.querySelectorAll(".our-stores .stores"),b=document.body.querySelector(".modal-window"),c=b.querySelector(".modal-close");return{openWindow:function(){return a.forEach(a=>a.addEventListener("click",c=>{let d=a.querySelector(".more"),e=a.querySelector(".img-store");(c.target==d||c.target==e)&&(document.body.style.overflowY="hidden",b.style.display="block")})),this},closeWindow:function(){c.addEventListener("click",()=>(document.body.style.overflowY="auto",b.style.display="none",this))}}}();
-
