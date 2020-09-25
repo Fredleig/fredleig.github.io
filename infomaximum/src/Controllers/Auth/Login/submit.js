8f216ecba8e5c65f530344d7f8145635a31e0de5
Fredleig <Kentis-can@protonmail.com>
2020-09-25 12:18:36 +0300

    deleted test task infomaximum

--- infomaximum/src/Controllers/Auth/Login/submit.js
+++
@@ -1,7 +0,0 @@
-import Database from "../../../Services/Database/Database";
-
-export const submitForm = (props, data) => {
-  return props.submitForm({
-    variables: {firstName: data.firstName, lastName: data.lastName}
-  })
-};
