13054468ab7546c9527ac214ece4ec8543be7153
Fredleig <Kentis-can@protonmail.com>
2020-11-27 16:25:04 +0300

    remove

--- kodmark-test/src/services/getData.ts
+++
@@ -1,4 +0,0 @@
-export const getData = (value?: string) => {
-    return fetch(`https://api.giphy.com/v1/gifs/random?api_key=gTJAO48YcpmrADUyo4opy4ES4g7iDBxx&tag=${value}`)
-    .then(res => res.json())
-}
