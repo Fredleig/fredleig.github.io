3d1e5f689b6d4f581f25c8f6cfb493a1e0ea2eca
Fredleig <Kentis-can@protonmail.com>
2021-01-25 23:55:55 +0300

    remove

--- zigmund_test/src/saga/index.ts
+++
@@ -1,6 +0,0 @@
-import { all } from "redux-saga/effects";
-import watcherLoadData from "./RequestGet/RequestGet";
-
-export default function* watchers() {
-  yield all([watcherLoadData()])
-}
--- zigmund_test/src/store/index.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { getRequestReducer } from "./RequestGet/RequestGet";
-
-const reducers = combineReducers({
-  getRequestReducer
-})
-
-export default reducers;
